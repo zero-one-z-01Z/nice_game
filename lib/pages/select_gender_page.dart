fe959649bdc7542bb4d39f26bb5ff5847c8b6f3f
unknown <ziad.w.khedr@gmail.com>
2026-02-04 09:56:44 +0200

    updated start video and background image

--- lib/pages/select_gender_page.dart
+++ lib/pages/select_gender_page.dart
@@ -15,7 +15,7 @@ class SelectGenderPage extends StatelessWidget {
         child: DecoratedBox(
           decoration: const BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('assets/main_image.png'),
+              image: AssetImage('assets/main_image.jpeg'),
               fit: BoxFit.fill,
             ),
           ),
